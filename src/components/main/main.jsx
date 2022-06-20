5271d97a4bc2f041189763499d0da47f64b08a85
Tomivan <gracejunkie20@gmail.com>
2022-06-20 21:44:37 +0100

    made changes to the home page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 import Background from "../../assets/images/background.png";
 import Cyberstick from "../../assets/images/cyberstick.png";
 import "./main.css";
@@ -8,10 +9,13 @@ const Main = () => {
         <div className="main">
             <img src={Background} alt="" className="background"/>
             <div className="main-info">
-                <h1>Think Beyond Smart,<br /> <span className="gradient">Think Cyberstick</span></h1>
-                <p>A multi purpose device that offers users to stream content with access to<br /> applications
-                    that allows users to work on the go
-                </p>
+                <div className="left">
+                    <h1>Think Beyond Smart,<br /> <span className="gradient">Think Cyberstick</span></h1>
+                    <p>A multi purpose device that offers users to stream content with access to<br /> applications
+                        that allows users to work on the go
+                    </p>
+                    <button className="buy-now"><Link to="/bag" className="link">Buy Now</Link></button>
+                </div>
                 <img src={Cyberstick} alt="" className="cyberstick"/>
             </div>
         </div>
